92e42c0348d339bf52de9d3420a59900b7ce0df5
Abdelrhman <abdogomaa1102@gmail.com>
2023-10-17 22:19:11 +0300

    fix: user model columns

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,4 +1,6 @@
 import { Model } from 'objection';
+import { Post } from 'src/post/models/post.model';
+import { UserRoleEnum } from '../user.enum';
 
 export class User extends Model {
   static get tableName(): string {
@@ -6,7 +8,50 @@ export class User extends Model {
   }
 
   id!: number;
-  name!: string;
+  firstName!: string;
+  lastName!: string;
   email!: string;
   password!: string;
+  phone?: string;
+  nation?: string;
+  role?: 'USER' | 'ADMIN';
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  // static get jsonSchema() {
+  //   return {
+  //     type: 'object',
+  //     required: ['firstName', 'lastName', 'email', 'password'],
+
+  //     properties: {
+  //       id: { type: 'integer' },
+  //       firstName: { type: 'string', minLength: 1, maxLength: 255 },
+  //       lastName: { type: 'string', minLength: 1, maxLength: 255 },
+  //       email: { type: 'string', minLength: 1,maxLength: 255 },
+  //       password: { type: 'string', minLength: 1, maxLength: 255 },
+  //       phone: { type: 'string', minLength: 1, maxLength: 255 },
+  //       nation: { type: 'string', minLength: 1, maxLength: 255 },
+  //       role: { type: 'enum', minLength: 1, maxLength: 255 },
+  //     }
+  //   };
+  // }
+
+  static get relationMappings() {
+    return{
+      posts: {
+				relation: Model.HasManyRelation,
+				modelClass: Post,
+				join: {
+					from: 'users.id',
+					to: 'posts.userId'
+				}
+			}
+		}
+	}
 }
---
+++ src/user/user.enum.ts
@@ -0,0 +1,11 @@
+export enum UserRoleEnum {
+    ADMIN = 'ADMIN',
+    EDITOR = 'EDITOR',
+    USER = 'USER'
+}
+
+export const UserRoleArray = [
+    'ADMIN',
+    'EDITOR',
+    'USER'
+]
