ebade159dd24be45e1bad0183f6ad8cb1f999140
Abdelrhman <abdogomaa1102@gmail.com>
2023-10-17 22:18:11 +0300

    feat: input validation

---
+++ src/auth/validation-regex.ts
@@ -0,0 +1,2 @@
+export const LangRegex =
+  /^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z-_]*$/; // Arabic and English Characters only
--- src/user/input/register.input.ts
+++ src/user/input/register.input.ts
@@ -1,15 +1,44 @@
-import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { LangRegex } from "../../auth/validation-regex";
+import { UserRoleEnum } from "../user.enum";
+import { ErrorCodeEnum } from "src/exceptions/error-code.enum";
 
 export class CreateUserInput {
-    @IsString()
     @IsNotEmpty()
-    name: string;
+    @Matches(LangRegex, { message: ErrorCodeEnum[ErrorCodeEnum.INVALID_NAME] })
+    @MinLength(2)
+    @MaxLength(25)
+    firstName: string;
 
-    @IsEmail()
     @IsNotEmpty()
+    @Matches(LangRegex, { message: ErrorCodeEnum[ErrorCodeEnum.INVALID_NAME] })
+    @MinLength(2)
+    @MaxLength(25)
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsEmail(
+      { allow_utf8_local_part: false },
+      { message: ErrorCodeEnum[ErrorCodeEnum.INVALID_EMAIL] }
+    )
     email: string;
 
     @IsString()
+    @Matches(/[A-Za-z]/, { message: ErrorCodeEnum[ErrorCodeEnum.INVALID_PASSWORD] })
+    @MinLength(8)
+    @MaxLength(26)
     @IsNotEmpty()
     password: string;
+
+    @IsOptional()
+    @IsString()
+    phone?: string;
+
+    @IsOptional()
+    @IsString()
+    nation?: string;
+    
+    @IsOptional()
+    @IsEnum(UserRoleEnum)
+    role?: UserRoleEnum;
 }
